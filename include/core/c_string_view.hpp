6e9e8009b4c3b4c8572fa9ff82c6af14ea97a7a0
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2025-09-19 10:05:55 +0300

    tune documentation

--- include/core/c_string_view.hpp
+++ include/core/c_string_view.hpp
@@ -32,23 +32,23 @@ public:
   /*!
     \brief Default constructor.
 
-    \post The string is empty and ready for use.
+    \post The string view is empty and ready for use.
   */
   constexpr CStringView() noexcept;
 
   /*!
-    \brief Constructs a copy of \a string.
+    \brief Constructs a copy of \a string view.
 
-    This constructor initializes a string by copying the C-style string pointer from another \a string.
+    This constructor initializes a string view by copying the C-style string pointer from another \a string view.
 
-    \param string The source string to copy C-style string pointer from.
+    \param string The source string view to copy C-style string pointer from.
 
-    \pre The source \a string must be valid and properly initialized.
+    \pre The source \a string view must be valid and properly initialized.
   */
   constexpr CStringView(const CStringView & string) noexcept;
 
   /*!
-    \brief Constructs a CStringView from a C-style \a string.
+    \brief Constructs a string view from a C-style \a string view.
 
     Creates a new CStringView object that wraps the provided C-style \a string pointer.
 
@@ -60,31 +60,31 @@ public:
   constexpr CStringView(const char * string) noexcept;
 
   /*!
-    \brief Destructor for the string.
+    \brief Destructor for the string view.
 
     \note Since the Destructor does not manage dynamic memory, no special cleanup is required.
   */
   constexpr ~CStringView() noexcept = default;
 
   /*!
-    \brief Copy assigns other \a string to this string.
+    \brief Copy assigns other \a string view to this string view.
 
-    This operator assigns the C-style string pointer of another \a string to this string.
+    This operator assigns the C-style string pointer of another \a string view to this string view.
 
-    \param string The source string to copy C-style string pointer from.
+    \param string The source string view to copy C-style string pointer from.
 
-    \return A reference to this string after assignment.
+    \return A reference to this string view after assignment.
   */
   constexpr CStringView & operator=(const CStringView & string) noexcept;
 
   /*!
-    \brief Copy assigns the C \a string to this string.
+    \brief Copy assigns the C \a string to this string view.
 
-    This operator assigns a new C \a string to this string.
+    This operator assigns a new C \a string to this string view.
 
     \param string The C-style string to assign.
 
-    \return A reference to this string after assignment.
+    \return A reference to this string view after assignment.
 
     \pre The source C \a string must not be null.
     \pre The source C \a string must be null-terminated.
@@ -92,13 +92,13 @@ public:
   constexpr CStringView & operator=(const char * string) noexcept;
 
   /*!
-    \brief Copy assigns other \a string to this string.
+    \brief Copy assigns other \a string view to this string view.
 
-    This method assigns the C-style string pointer of another \a string to this string.
+    This method assigns the C-style string pointer of another \a string view to this string view.
 
-    \param string The source string to copy C-style string pointer from.
+    \param string The source string view to copy C-style string pointer from.
 
-    \return A reference to this string after assignment.
+    \return A reference to this string view after assignment.
 
     \note This method is equivalent to the copy assignment operator.
 
@@ -107,13 +107,13 @@ public:
   constexpr CStringView & assign(const CStringView & string) noexcept;
 
   /*!
-    \brief Copy assigns the C \a string to this string.
+    \brief Copy assigns the C \a string to this string view.
 
-    This method assigns a new C \a string to this string.
+    This method assigns a new C \a string to this string view.
 
     \param string The C-style string to assign.
 
-    \return A reference to this string after assignment.
+    \return A reference to this string view after assignment.
 
     \pre The source C \a string must not be null.
     \pre The source C \a string must be null-terminated.
@@ -125,15 +125,15 @@ public:
   constexpr CStringView & assign(const char * string) noexcept;
 
   /*!
-    \brief Access a character in the string at a given \a offset.
+    \brief Access a character in the string view at a given \a offset.
 
-    This method provides read-only access to a character at the specified \a offset within the string.
+    This method provides read-only access to a character at the specified \a offset within the string view.
 
-    \param offset The offset of the character to access in the string.
+    \param offset The offset of the character to access in the string view.
 
     \return A const reference to the character at the specified \a offset.
 
-    \pre The \a offset should be less than the current string size.
+    \pre The \a offset should be less than the current string view size.
 
     \note The returned reference is read-only and cannot modify the character.
     \note This method is equivalent to the subscript operator.
@@ -143,92 +143,92 @@ public:
   [[nodiscard]] constexpr const char & at(std::size_t offset) const noexcept;
 
   /*!
-    \brief Access a character in the string at a given \a offset.
+    \brief Access a character in the string view at a given \a offset.
 
-    This operator provides read-only access to a character at the specified \a offset within the string.
+    This operator provides read-only access to a character at the specified \a offset within the string view.
 
-    \param offset The offset of the character to access in the string.
+    \param offset The offset of the character to access in the string view.
 
     \return A const reference to the character at the specified \a offset.
 
-    \pre The \a offset should be less than the current string size.
+    \pre The \a offset should be less than the current string view size.
 
     \note The returned reference is read-only and cannot modify the character.
   */
   [[nodiscard]] constexpr const char & operator[](std::size_t offset) const noexcept;
 
   /*!
-    \brief Returns a const reference to the first character of the string.
+    \brief Returns a const reference to the first character of the string view.
 
-    This method provides read-only access to the first character of the string.
+    This method provides read-only access to the first character of the string view.
 
-    \return A const reference to the first character of the string.
+    \return A const reference to the first character of the string view.
 
     \note The returned reference is read-only and cannot modify the character.
   */
   [[nodiscard]] constexpr const char & front() const noexcept;
 
   /*!
-    \brief Returns a const reference to the last character of the string.
+    \brief Returns a const reference to the last character of the string view.
 
-    This method provides read-only access to the last character of the string.
+    This method provides read-only access to the last character of the string view.
 
-    \return A const reference to the last character of the string.
+    \return A const reference to the last character of the string view.
 
-    \pre The string must not be empty.
+    \pre The string view must not be empty.
 
     \note The returned reference is read-only and cannot modify the character.
   */
   [[nodiscard]] constexpr const char & back() const noexcept;
 
   /*!
-    \brief Returns a constant pointer to the data of the string.
+    \brief Returns a constant pointer to the data of the string view.
 
-    This method returns a constant pointer to the internal character array that stores the string data. The returned
-    pointer provides read-only access to the string contents and can be used for low-level operations.
+    This method returns a constant pointer to the internal character array that stores the string view data. The
+    returned pointer provides read-only access to the string view contents and can be used for low-level operations.
 
     \return A constant pointer to the internal character array.
 
     \note The returned pointer points to a null-terminated character array.
-    \note The returned pointer is read-only and cannot modify the string contents.
+    \note The returned pointer is read-only and cannot modify the string view contents.
   */
   [[nodiscard]] constexpr const char * data() const noexcept;
 
   /*!
-    \brief Returns a constant pointer to the C string representation of this string.
+    \brief Returns a constant pointer to the C string representation of this string view.
 
-    This method returns a constant pointer to the C string representation of this string. The returned pointer provides
-    read-only access to the string contents and can be used with C functions that require a char pointer.
+    This method returns a constant pointer to the C string representation of this string view. The returned pointer
+    provides read-only access to the string view contents and can be used with C functions that require a char pointer.
 
-    \return A constant pointer to the C string representation of this string.
+    \return A constant pointer to the C string representation of this string view.
 
-    \note The returned pointer points to a null-terminated string.
-    \note The returned pointer is read-only and cannot modify the string contents.
+    \note The returned pointer points to a null-terminated string view.
+    \note The returned pointer is read-only and cannot modify the string view contents.
     \note This method is equivalent to data() const method.
   */
   [[nodiscard]] constexpr const char * c_str() const noexcept;
 
   /*!
-    \brief Checks if the string is empty.
+    \brief Checks if the string view is empty.
 
-    This method checks if the string is empty, i.e. its size is zero. An empty string contains no characters and has a
-    length of zero.
+    This method checks if the string view is empty, i.e. its size is zero. An empty string view contains no characters
+    and has a length of zero.
 
-    \return True if the string is empty, false otherwise.
+    \return True if the string view is empty, false otherwise.
 
-    \note An empty string has size zero.
-    \note An empty string still contains a null terminator.
+    \note An empty string view has size zero.
+    \note An empty string view still contains a null terminator.
     \note This method is equivalent to the expression: ```size() == 0```.
   */
   [[nodiscard]] constexpr bool empty() const noexcept;
 
   /*!
-    \brief Returns the size of the string.
+    \brief Returns the size of the string view.
 
-    This method returns the current number of characters in the string, excluding the terminating null character. The
-    size represents the actual length of the string content.
+    This method returns the current number of characters in the string view, excluding the terminating null character.
+    The size represents the actual length of the string view content.
 
-    \return The number of characters in the string, excluding the terminating null character.
+    \return The number of characters in the string view, excluding the terminating null character.
 
     \note This method is equivalent to length() method.
   */
@@ -242,7 +242,7 @@ public:
     number of Unicode characters rather than bytes. The method validates UTF-8 encoding and counts only complete,
     well-formed Unicode characters.
 
-    \return The number of Unicode characters in the string, excluding the terminating null character.
+    \return The number of Unicode characters in the string view, excluding the terminating null character.
 
     \note For ASCII strings, utf8_size() equals size().
     \note For UTF-8 strings, utf8_size() may be less than size().
@@ -252,12 +252,12 @@ public:
   [[nodiscard]] inline std::size_t utf8_size() const noexcept;
 
   /*!
-    \brief Returns the size of the string.
+    \brief Returns the size of the string view.
 
-    This method returns the current number of characters in the string, excluding the terminating null character. The
-    length represents the actual length of the string content.
+    This method returns the current number of characters in the string view, excluding the terminating null character.
+    The length represents the actual length of the string view content.
 
-    \return The number of characters in the string, excluding the terminating null character.
+    \return The number of characters in the string view, excluding the terminating null character.
 
     \note This method is equivalent to size() method.
 
@@ -266,25 +266,25 @@ public:
   [[nodiscard]] constexpr std::size_t length() const noexcept;
 
   /*!
-    \brief Returns the maximum size of the string.
+    \brief Returns the maximum observable size of this string view.
 
-    This method returns the maximum possible size for the string. The maximum size represents the size of the allocated
-    buffer.
+    This method returns the maximum possible size for the string view. The maximum size represents the size of the
+    allocated buffer.
 
-    \return The maximum number of characters in the string, excluding the terminating null character.
+    \return The maximum number of characters in the string view, excluding the terminating null character.
 
-    \note This method is equivalent to capacity() method.
+    \note This method is equivalent to size() method.
   */
   [[nodiscard]] constexpr std::size_t max_size() const noexcept;
 
   /*!
-    \brief Returns the capacity of the string.
+    \brief Returns the capacity of the string view.
 
-    This method returns the capacity of the allocated buffer for the string.
+    This method returns the capacity of the allocated buffer for the string view.
 
-    \return The capacity of the string in characters, excluding the terminating null character.
+    \return The capacity of the string view in characters, excluding the terminating null character.
 
-    \note This method is equivalent to max_size() method.
+    \note This method is equivalent to size() method.
   */
   [[nodiscard]] constexpr std::size_t capacity() const noexcept;
 
@@ -302,7 +302,7 @@ private:
   /// Pointer to the wrapped C-style string
   const char * _data;
 
-  /// Static empty string used as default value for null pointers
+  /// Static empty C-style string used as default value for null pointers
   static constexpr char _emptyString[]{'\0'};
 };
 
