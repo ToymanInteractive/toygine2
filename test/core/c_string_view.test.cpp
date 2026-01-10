199cd627fc753f7737f9474e3720b3209face9d9
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-01-10 10:14:07 +0300

    cleanup

--- test/core/c_string_view.test.cpp
+++ test/core/c_string_view.test.cpp
@@ -26,11 +26,6 @@ namespace toy {
 
 TEST_CASE("CStringView constructors", "[core][c_string_view]") {
   SECTION("Object size") {
-    constexpr CStringView string("Example String");
-
-    REQUIRE(sizeof(CStringView) == sizeof(char *));
-
-    // Compile-time checks
     STATIC_REQUIRE(sizeof(CStringView) == sizeof(char *));
   }
 
