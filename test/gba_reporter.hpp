876a188cb2e2298d34dc511cd0d19965a7b4da01
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-02-23 17:57:17 +0300

    fixes

--- test/gba_reporter.hpp
+++ test/gba_reporter.hpp
@@ -1,7 +1,41 @@
+//
+// Copyright (c) 2025-2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   gba_reporter.hpp
+  \brief  Minimal DocTest reporter for Nintendo GBA.
+*/
+
+#ifndef TEST_GBA_REPORTER_HPP_
+#define TEST_GBA_REPORTER_HPP_
+
 #include <doctest/doctest.h>
 
+/*!
+  \struct GBAReporter
+  \brief No-op DocTest reporter implementing \c doctest::IReporter.
+
+  Used when running tests on targets where console or file output is unavailable (e.g. GBA). All virtual methods are
+  overridden with empty bodies; test execution still runs, but no output is produced.
+*/
 struct GBAReporter : public doctest::IReporter {
-  GBAReporter([[maybe_unused]] const doctest::ContextOptions & in_options) {}
+  explicit GBAReporter([[maybe_unused]] const doctest::ContextOptions & in_options) {}
 
   void test_run_start() override {}
 
@@ -20,7 +54,12 @@ struct GBAReporter : public doctest::IReporter {
   void test_case_skipped([[maybe_unused]] const doctest::TestCaseData & data) override {}
 
   void report_query([[maybe_unused]] const doctest::QueryData & data) override {}
+
   void subcase_start([[maybe_unused]] const doctest::SubcaseSignature & signature) override {}
+
   void subcase_end() override {}
+
   void test_case_reenter([[maybe_unused]] const doctest::TestCaseData & data) override {}
 };
+
+#endif // TEST_GBA_REPORTER_HPP_
