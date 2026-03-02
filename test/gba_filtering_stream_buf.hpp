b7136fc470c1d1cb38c6af24841c4ace042e0094
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-03-02 20:04:25 +0300

    fix?

--- test/gba_filtering_stream_buf.hpp
+++ test/gba_filtering_stream_buf.hpp
@@ -1,9 +1,37 @@
+//
+// Copyright (c) 2025-2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   gba_reporter.hpp
+*/
+
+#ifndef TEST_GBA_FILTERING_STREAM_BUF_HPP_
+#define TEST_GBA_FILTERING_STREAM_BUF_HPP_
+
 #include <iostream>
 #include <streambuf>
 
 class GbaFilteringStreamBuf final : public std::streambuf {
 protected:
-  int_type overflow(int_type c) override;
-  std::streamsize xsputn(const char * s, std::streamsize n) override;
-  int sync() override;
+  int_type overflow(int_type c) noexcept override;
+  std::streamsize xsputn(const char * s, std::streamsize n) noexcept override;
+  int sync() noexcept override;
 };
+
+#endif // TEST_GBA_FILTERING_STREAM_BUF_HPP_
