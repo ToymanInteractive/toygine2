dc640e349b8172e85b447ab0da3ae9d038da5918
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-04-08 05:53:32 +0300

    rename code quality

--- tools/builder/check_code_format.sh
+++ tools/builder/check_code_format.sh
@@ -27,8 +27,6 @@
 
 set -e -o pipefail
 
-clang-format --version
-
 # Get all modified files in the current branch compared to base branch
 FILES_TO_CHECK=$(git diff --name-only HEAD^ | (grep -E ".*\.(cpp|cc|c\+\+|cxx|c|h|hpp|inl|java|js)$" || true) \
                                             | (grep -v "^src/thirdparty/.*/.*" || true))
