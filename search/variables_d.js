2b067f6ad811006e98cd1618f58ac1549a8a216a
dkrutskikh <dkrutskikh@users.noreply.github.com>
2026-04-26 13:12:07 +0000

    deploy: b6a73a8182959aefe67d847ab6828cc8d71bf3bf

--- search/variables_d.js
+++ search/variables_d.js
@@ -5,6 +5,6 @@ var searchData=
   ['radiuses_2',['radiuses',['../d9/dd1/classtoy_1_1geometry_1_1_ellipse.html#a797c69d9f316e5a20e5f371b6d581736',1,'toy::geometry::Ellipse']]],
   ['radix_3',['radix',['../d4/d44/classstd_1_1numeric__limits_3_01toy_1_1math_1_1fixed_3_01_base_00_01_intermediate_00_01_fraction_00_01_rounding_01_4_01_4.html#a4e315a22a0727c409b1e0582d45950c3',1,'std::numeric_limits&lt; toy::math::fixed&lt; Base, Intermediate, Fraction, Rounding &gt; &gt;']]],
   ['rem_4',['rem',['../da/d8f/structtoy_1_1_div_mod10.html#a49fb8e6a78e80c23dfb831335db0bbdb',1,'toy::DivMod10']]],
-  ['revision_5',['revision',['../d7/d33/structtoy_1_1application_1_1_version.html#adcca1886dcfda18458d39652bb602fb4',1,'toy::application::Version']]],
+  ['revision_5',['revision',['../d7/d33/structtoy_1_1application_1_1_version.html#a1c4247fbb7eac4c15835dcc164f9bf31',1,'toy::application::Version']]],
   ['round_5fstyle_6',['round_style',['../d4/d44/classstd_1_1numeric__limits_3_01toy_1_1math_1_1fixed_3_01_base_00_01_intermediate_00_01_fraction_00_01_rounding_01_4_01_4.html#ae61bb355a9776342cfed55427ee331d9',1,'std::numeric_limits&lt; toy::math::fixed&lt; Base, Intermediate, Fraction, Rounding &gt; &gt;']]]
 ];
