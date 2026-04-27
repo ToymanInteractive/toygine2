665540966673da0a862251cfb1d641c0bb625490
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-04-27 05:21:14 +0000

    feat: introduce macOS implementation of CoreApplication (#397)

---
+++ src/platforms/macos/application/core_application_macos.cpp
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2025-2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   core_application_macos.cpp
+  \brief  macOS implementations of \ref toy::application::CoreApplication via POSIX APIs.
+*/
+
+#include <cerrno>
+#include <ctime>
+
+#include <unistd.h>
+
+#include "application.hpp"
+
+namespace toy::application {
+
+uint32_t CoreApplication::pid() const noexcept {
+  static_assert(sizeof(std::uint32_t) >= sizeof(pid_t), "uint32_t must be large enough to hold pid_t");
+
+  return getpid();
+}
+
+void CoreApplication::sleep(size_t milliseconds) const noexcept {
+  timespec ts = {.tv_sec  = static_cast<time_t>(milliseconds / 1000),
+                 .tv_nsec = static_cast<long>((milliseconds % 1000) * 1000000)};
+
+  while (nanosleep(&ts, &ts) == -1 && errno == EINTR) {
+    // Resume sleeping for the remaining duration after signal interruption.
+  }
+}
+
+} // namespace toy::application
