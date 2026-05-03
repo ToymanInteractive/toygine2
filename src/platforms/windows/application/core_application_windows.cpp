9f6fbe7218589f761670e6fc059cbc4f2a97450b
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-05-03 07:56:28 +0300

    fixes

--- src/platforms/windows/application/core_application_windows.cpp
+++ src/platforms/windows/application/core_application_windows.cpp
@@ -38,7 +38,9 @@ uint32_t CoreApplication::pid() const noexcept {
 }
 
 void CoreApplication::sleep(size_t milliseconds) const noexcept {
-  assert_message(milliseconds <= UINT32_MAX, "milliseconds must be less than or equal to UINT32_MAX");
+  if constexpr (sizeof(size_t) > sizeof(DWORD)) {
+    assert_message(milliseconds <= UINT32_MAX, "milliseconds must be less than or equal to UINT32_MAX");
+  }
 
   Sleep(static_cast<DWORD>(milliseconds));
 }
