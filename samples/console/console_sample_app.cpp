9cf8b3ad0e6094b8d711143c8d7e4a2f4f4bdaac
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-08-23 17:31:10 +0300

    wii example

--- samples/console/console_sample_app.cpp
+++ samples/console/console_sample_app.cpp
@@ -159,7 +159,38 @@ int main() {
 
 #endif // __gamecube__
 
-#if !defined(__GBA__) && !defined(__NDS__) && !defined(__3DS__) && !defined(__SWITCH__) && !defined(__gamecube__)
+#ifdef __wii__
+
+#include <gccore.h>
+
+int main() {
+  VIDEO_Init();
+
+  auto * rmode = VIDEO_GetPreferredMode(NULL);
+
+  auto * framebuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
+  console_init(framebuffer, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+
+  VIDEO_Configure(rmode);
+  VIDEO_SetNextFramebuffer(framebuffer);
+  VIDEO_SetBlack(false);
+  VIDEO_Flush();
+  VIDEO_WaitVSync();
+  if (rmode->viTVMode & VI_NON_INTERLACE)
+    VIDEO_WaitVSync();
+
+  printf("Hello world from Console sample app");
+
+  while (SYS_MainLoop())
+    VIDEO_WaitVSync();
+
+  return 0;
+}
+
+#endif // __wii__
+
+#if !defined(__GBA__) && !defined(__NDS__) && !defined(__3DS__) && !defined(__SWITCH__) && !defined(__gamecube__)      \
+  && !defined(__wii__)
 
 #include <iostream>
 
@@ -171,4 +202,4 @@ int main(int argc, char * argv[]) noexcept {
   return EXIT_SUCCESS;
 }
 
-#endif // __GBA__
+#endif
