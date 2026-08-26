798e866e8d1ebbd3ed19719274c4872954e74e74
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-08-26 21:04:32 +0300

    msvc

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -28,7 +28,7 @@ if (TOYGINE_TARGET_PLATFORM STREQUAL "Windows Desktop")
 
   # MSVC Compiler Options
   # https://learn.microsoft.com/en-nz/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170#optimization
-  # last option is /Gy
+  # last option is /jumptablerdata
 
   # MSVC Linker Options
   # https://learn.microsoft.com/en-nz/cpp/build/reference/linker-options?view=msvc-170
@@ -63,11 +63,11 @@ if (TOYGINE_TARGET_PLATFORM STREQUAL "Windows Desktop")
       string(APPEND CMAKE_C_FLAGS   " /arch:SSE2 /fpcvt:IA")
       string(APPEND CMAKE_CXX_FLAGS " /arch:SSE2 /fpcvt:IA")
 
-      string(APPEND CMAKE_C_FLAGS_RELWITHDEBINFO          " /favor:blend /Gv /homeparams")
-      string(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO        " /favor:blend /Gv /homeparams")
+      string(APPEND CMAKE_C_FLAGS_RELWITHDEBINFO          " /favor:blend /Gv /homeparams /jumptablerdata")
+      string(APPEND CMAKE_CXX_FLAGS_RELWITHDEBINFO        " /favor:blend /Gv /homeparams /jumptablerdata")
 
-      string(APPEND CMAKE_C_FLAGS_RELEASE                 " /favor:blend /Gv")
-      string(APPEND CMAKE_CXX_FLAGS_RELEASE               " /favor:blend /Gv")
+      string(APPEND CMAKE_C_FLAGS_RELEASE                 " /favor:blend /Gv             /jumptablerdata")
+      string(APPEND CMAKE_CXX_FLAGS_RELEASE               " /favor:blend /Gv             /jumptablerdata")
 
       string(APPEND CMAKE_EXE_LINKER_FLAGS_RELWITHDEBINFO "")
 
