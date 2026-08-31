013f0e2ae56172387a46fcfb27f2f12f30d405e0
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-08-31 20:14:05 +0300

    fix md

---
+++ extern/clownmdsdk/include/string.h
@@ -0,0 +1,19 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__attribute__((visibility("default"))) void* memcpy(void *dest, const void *src, size_t count);
+__attribute__((visibility("default"))) void* memmove(void *dest, const void *src, size_t count);
+__attribute__((visibility("default"))) void* memset(void *dest, int ch, size_t count);
+__attribute__((visibility("default"))) char* strncpy(char *dest, const char *src, size_t count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* STRING_H */
