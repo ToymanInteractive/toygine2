220216fc6afee6e478d3886b996cfb7ee3b399d6
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-09-06 18:24:42 +0300

    feat: application version

---
+++ include/application.hpp
@@ -0,0 +1,49 @@
+//
+// Copyright (c) 2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   application.hpp
+  \brief  Umbrella header for the engine application module.
+
+  Single public entry point for the application module: aggregates its public headers into namespace
+  \ref toy::application.
+
+  \note Include this header only; do not include internal headers directly.
+*/
+
+#ifndef INCLUDE_APPLICATION_HPP_
+#define INCLUDE_APPLICATION_HPP_
+
+#include "core.hpp"
+
+/*!
+  \namespace toy::application
+
+  \brief Application-level types, among them the version a build reports about itself.
+*/
+
+#include "application/version.hpp"
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#include "application/version.inl"
+
+//----------------------------------------------------------------------------------------------------------------------
+
+#endif // INCLUDE_APPLICATION_HPP_
---
+++ include/application/version.inl
@@ -0,0 +1,47 @@
+//
+// Copyright (c) 2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   version.inl
+  \brief  Inline implementations for \ref toy::application::Version ordering.
+
+  \note Included by application.hpp only; do not include this file directly.
+*/
+
+#ifndef INCLUDE_APPLICATION_VERSION_INL_
+#define INCLUDE_APPLICATION_VERSION_INL_
+
+namespace toy::application {
+
+constexpr strong_ordering operator<=>(const Version & lhs, const Version & rhs) noexcept {
+  if (const auto cmp = lhs.major <=> rhs.major; cmp != strong_ordering::equal)
+    return cmp;
+
+  if (const auto cmp = lhs.minor <=> rhs.minor; cmp != strong_ordering::equal)
+    return cmp;
+
+  if (const auto cmp = lhs.patch <=> rhs.patch; cmp != strong_ordering::equal)
+    return cmp;
+
+  return lhs.revision <=> rhs.revision;
+}
+
+} // namespace toy::application
+
+#endif // INCLUDE_APPLICATION_VERSION_INL_
