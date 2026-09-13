5bf1929332631e1b0dfd95753b4a5824bb95e13e
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-09-13 05:37:41 +0300

    task6

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -31,10 +31,10 @@ cmake_minimum_required(VERSION 3.27.0 FATAL_ERROR)
 # Based on https://en.wikipedia.org/wiki/Xcode#Toolchain_versions
 # Clang 21.1.0 documentation.    https://llvm.github.io/www-releases/21.1.0/tools/clang/docs/UsersManual.html
 # Clang 21.1.0 diagnostic flags. https://llvm.github.io/www-releases/21.1.0/tools/clang/docs/DiagnosticsReference.html
-# last option is -Wabstract-vbase-init
+# last option is -Wall
 
-set(CLANG_CMAKE_C_FLAGS                  "-Werror")
-set(CLANG_CMAKE_CXX_FLAGS                "-Werror -Wabstract-vbase-init")
+set(CLANG_CMAKE_C_FLAGS                  "-Werror                       -Wall")
+set(CLANG_CMAKE_CXX_FLAGS                "-Werror -Wabstract-vbase-init -Wall")
 
 set(CLANG_CMAKE_C_FLAGS_DEBUG            "")
 set(CLANG_CMAKE_CXX_FLAGS_DEBUG          "")
@@ -65,13 +65,13 @@ if (TOYGINE_TARGET_PLATFORM STREQUAL "Windows Desktop")
 
     # MSVC Compiler Options
     # https://learn.microsoft.com/en-nz/cpp/build/reference/compiler-options-listed-by-category?view=msvc-170#optimization
-    # last option is /Zc:referenceBinding
+    # last option is /Zc:rvalueCast
 
     # MSVC Linker Options
     # https://learn.microsoft.com/en-nz/cpp/build/reference/linker-options?view=msvc-170
 
     set(CMAKE_C_FLAGS                  "/EHsc /GA /GR- /GS /guard:cf /volatile:iso /DWIN32 /D_WINDOWS /DUNICODE /D_UNICODE /permissive- /std:c17       /Zc:__STDC__                                                                 /Zc:forScope                                                     /Zc:inline")
-    set(CMAKE_CXX_FLAGS                "/EHsc /GA /GR- /GS /guard:cf /volatile:iso /DWIN32 /D_WINDOWS /DUNICODE /D_UNICODE /permissive- /std:c++latest /Zc:__cplusplus /Zc:alignedNew /Zc:enumTypes /Zc:externC /Zc:externConstexpr /Zc:forScope /Zc:gotoScope /Zc:hiddenFriend /Zc:implicitNoexcept /Zc:inline /Zc:lambda /Zc:noexceptTypes /Zc:nrvo /Zc:preprocessor /Zc:referenceBinding")
+    set(CMAKE_CXX_FLAGS                "/EHsc /GA /GR- /GS /guard:cf /volatile:iso /DWIN32 /D_WINDOWS /DUNICODE /D_UNICODE /permissive- /std:c++latest /Zc:__cplusplus /Zc:alignedNew /Zc:enumTypes /Zc:externC /Zc:externConstexpr /Zc:forScope /Zc:gotoScope /Zc:hiddenFriend /Zc:implicitNoexcept /Zc:inline /Zc:lambda /Zc:noexceptTypes /Zc:nrvo /Zc:preprocessor /Zc:referenceBinding /Zc:rvalueCast")
 
     set(CMAKE_C_FLAGS_DEBUG            "/Od /Ob0 /Oi-     /Oy- /fp:strict /fp:except  /Gd /GF- /GL- /Gw- /Gy- /RTC1 /D_DEBUG")
     set(CMAKE_CXX_FLAGS_DEBUG          "/Od /Ob0 /Oi-     /Oy- /fp:strict /fp:except  /Gd /GF- /GL- /Gw- /Gy- /RTC1 /D_DEBUG")
@@ -137,8 +137,8 @@ if (TOYGINE_TARGET_PLATFORM STREQUAL "Windows Desktop")
 
     set(CMAKE_MSVC_RUNTIME_LIBRARY "MultiThreaded$<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:Debug>")
 
-    string(APPEND CMAKE_C_FLAGS                   " /nologo /Zc:wchar_t /Zc:preprocessor /Zc:rvalueCast")
-    string(APPEND CMAKE_CXX_FLAGS                 " /nologo /Zc:wchar_t /Zc:preprocessor /Zc:rvalueCast")
+    string(APPEND CMAKE_C_FLAGS                   " /nologo /Zc:wchar_t /Zc:preprocessor")
+    string(APPEND CMAKE_CXX_FLAGS                 " /nologo /Zc:wchar_t /Zc:preprocessor")
 
     string(APPEND CMAKE_C_FLAGS_DEBUG             " /diagnostics:caret   /sdl      /MTd /MP")
     string(APPEND CMAKE_CXX_FLAGS_DEBUG           " /diagnostics:caret   /sdl      /MTd /MP")
