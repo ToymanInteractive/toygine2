8782ecac477260331b4404ee919518b824497b5c
Dmitry Krutskikh <dmitry.krutskikh@gmail.com>
2026-09-20 06:33:22 +0300

    editor test

---
+++ editor/tests/editor.test.cpp
@@ -0,0 +1,37 @@
+//
+// Copyright (c) 2026 Toyman Interactive
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and / or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+/*!
+  \file   string_view.test.cpp
+  \brief  Unit tests for \ref toy::StringView.
+*/
+
+#include <doctest/doctest.h>
+
+#include "core.hpp"
+
+namespace toy {
+
+// The aliases the view publishes, and the type each one has to name.
+TEST_CASE("editor/string_view/types") {
+  const StringView empty;
+  CHECK(empty.begin() == nullptr);
+}
+
+} // namespace toy
